e25a51f58e555818e9e62c3d5b25c18da1d5b788
2impaoo <baoga271104@gmail.com>
2025-03-09 09:29:16 +0700

    [fix] first page

--- ditme/ditme/wwwroot/js/site.js
+++ ditme/ditme/wwwroot/js/site.js
@@ -1,18 +1,23 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
-    let oneWay = document.getElementById("one-way");
-    let roundWay = document.getElementById("round-way");
-    let checkOutGroup = document.getElementById("check-out-group");
+    const oneWay = document.getElementById("one-way");
+    const roundWay = document.getElementById("round-way");
+    const checkOutGroup = document.getElementById("check-out-group");
+    
 
-    function toggleCheckOut() {
+    oneWay.addEventListener("change", function () {
         if (oneWay.checked) {
-            checkOutGroup.style.display = "none";
-        } else {
-            checkOutGroup.style.display = "flex";
+            checkOutGroup.classList.add("hidden");
         }
+    });
+
+    roundWay.addEventListener("change", function () {
+        if (roundWay.checked) {
+            checkOutGroup.classList.remove("hidden");
+        }
+    });
+
+    // Kiểm tra trạng thái ban đầu
+    if (oneWay.checked) {
+        checkOutGroup.classList.add("hidden");
     }
-
-    oneWay.addEventListener("change", toggleCheckOut);
-    roundWay.addEventListener("change", toggleCheckOut);
-
-    toggleCheckOut(); // Ensure initial state
 });
